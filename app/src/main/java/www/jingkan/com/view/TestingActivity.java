34330f4713fa83e4374f977ea56b8b5a8926f4f3
bobo19870225 <rain19870225@163.com>
2019-03-05 15:12:20 +0800

    完善探头检测功能

--- app/src/main/java/www/jingkan/com/view/TestingActivity.java
+++ app/src/main/java/www/jingkan/com/view/TestingActivity.java
@@ -1,28 +1,50 @@
 package www.jingkan.com.view;
 
+import android.view.MenuItem;
+
+import javax.inject.Inject;
+
+import androidx.lifecycle.ViewModelProviders;
 import www.jingkan.com.R;
 import www.jingkan.com.databinding.ActivityTestingBinding;
 import www.jingkan.com.util.CallbackMessage;
+import www.jingkan.com.util.bluetooth.BluetoothCommService;
+import www.jingkan.com.util.bluetooth.BluetoothUtil;
 import www.jingkan.com.view.base.DialogMVVMDaggerActivity;
 import www.jingkan.com.view_model.TestingViewModel;
-
-import androidx.lifecycle.ViewModelProviders;
 
 /**
  * Created by Sampson on 2018/12/21.
  * CPTTest
  */
 public class TestingActivity extends DialogMVVMDaggerActivity<TestingViewModel, ActivityTestingBinding> {
+    @Inject
+    BluetoothUtil bluetoothUtil;
+    @Inject
+    BluetoothCommService bluetoothCommService;
+
+
     @Override
     protected Object[] injectToViewModel() {
-        return new Object[0];
+        return new Object[]{mData, bluetoothUtil, bluetoothCommService};
     }
 
     @Override
     protected void setMVVMView() {
-
+        setToolBar("探头检测", R.menu.link);
+        mViewModel.singleLiveEvent.observe(this, aVoid -> showWaitDialog("正在连接蓝牙设备...", false, false));
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.link:
+                mViewModel.link();
+                return false;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
     @Override
     public TestingViewModel createdViewModel() {
         return ViewModelProviders.of(this).get(TestingViewModel.class);
--- app/src/main/java/www/jingkan/com/view_model/TestingViewModel.java
+++ app/src/main/java/www/jingkan/com/view_model/TestingViewModel.java
@@ -1,12 +1,15 @@
 package www.jingkan.com.view_model;
 
 import android.app.Application;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-
-import www.jingkan.com.view_model.base.BaseViewModel;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
+import www.jingkan.com.util.SingleLiveEvent;
+import www.jingkan.com.util.bluetooth.BluetoothCommService;
+import www.jingkan.com.util.bluetooth.BluetoothUtil;
+import www.jingkan.com.view_model.base.BaseViewModel;
 
 /**
  * Created by Sampson on 2018/12/21.
@@ -14,6 +17,10 @@ import androidx.lifecycle.MutableLiveData;
  */
 public class TestingViewModel extends BaseViewModel {
     public final MutableLiveData strData = new MutableLiveData();
+    public final SingleLiveEvent<Void> singleLiveEvent = new SingleLiveEvent<>();
+    private BluetoothUtil bluetoothUtil;
+    private BluetoothCommService bluetoothCommService;
+    private String mac;
 
     public TestingViewModel(@NonNull Application application) {
         super(application);
@@ -21,7 +28,9 @@ public class TestingViewModel extends BaseViewModel {
 
     @Override
     public void inject(Object... objects) {
-
+        mac = (String) objects[0];
+        bluetoothUtil = (BluetoothUtil) objects[1];
+        bluetoothCommService = (BluetoothCommService) objects[2];
     }
 
     @Override
@@ -32,5 +41,12 @@ public class TestingViewModel extends BaseViewModel {
     @Override
     public void clear() {
 
+    }
+
+    public void link() {
+        BluetoothDevice bluetoothDevice = bluetoothUtil.getBluetoothAdapter().getRemoteDevice(mac);
+        bluetoothCommService.connect(bluetoothDevice);
+        singleLiveEvent.call();
+//        getView().showWaitDialog("正在连接蓝牙设备...", false, false);
     }
 }
